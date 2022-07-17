f71c2e5982e98a49d5ae3cecb22272eed27b70f1
MohamedHossamElden <mohamedhossamelden2009@gmail.com>
2022-07-17 19:11:37 +0200

    Added Alarm

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 function updateTime() {
     const date = new Date();
-
+    
     const hour = (date.getHours());
     const minutes = (date.getMinutes());
     const seconds = (date.getSeconds());
@@ -12,4 +12,19 @@ function updateTime() {
 
 setInterval(updateTime, 1000);
 
-let txt = document.getElementById("clock");
+const aud = document.getElementById("alaSound");
+
+let txt = document.getElementById("clock"),
+    btn = document.getElementById("btn");
+
+btn.onclick = function () {
+    let alarmBtn = prompt("بعد كم دقيقة عايزه يرن؟؟"),
+        sec = ((alarmBtn * 60)* 1000);
+
+    console.log(sec);
+
+    setTimeout(() => {
+        aud.play();
+        alert("Alaaaaaaaarm!!");
+    }, sec);
+}
